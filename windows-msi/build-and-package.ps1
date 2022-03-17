bc1ce6b8c0d61f0f7993e1a8af691be692ff2845
Samuli Seppänen <samuli@openvpn.net>
2022-03-17 06:10:58 +0000

    windows-msi: fixes and improvements to build-and-package.ps1
    - Support openssl 1.1.1 and 3.x builds
    - Ensure that all dependencies are installed, upgraded and
      integrated in case something in provisioning failed or
      dependencies have changed
    - Reorder some commands so they make more sense
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- windows-msi/build-and-package.ps1
+++ windows-msi/build-and-package.ps1
@@ -1,8 +1,14 @@
-param([string] $basedir)
+param(
+    # Must be a directory with openvpn, openvpn-gui, vcpkg and
+    # openvpn-build side by side
+    [string] $basedir,
+    # Version of OpenSSL port to use ("openssl" or "openssl3")
+    [string] $openssl = "openssl3"
+    )
 
 ### Preparations
 if(-not($basedir)) {
-	Write-Host "Usage: build-and-package.ps1 -basedir <basedir>"
+    Write-Host "Usage: build-and-package.ps1 -basedir <basedir> [-openssl] <openssl|openssl3>"
     exit 1
 }
 
@@ -13,12 +19,12 @@ $basedir_exists = Test-Path $basedir
 
 if ($basedir_exists -ne $True) {
     Write-Host "ERROR: directory ${basedir} does not exist!"
-	exit 1
+    exit 1
 }
 
 if ((Test-Path "${PSScriptRoot}/build-and-package-env.ps1") -ne $True) {
-	Write-Host "ERROR: configuration file (build-and-package-env.ps1I is missing"
-	exit 1
+    Write-Host "ERROR: configuration file (build-and-package-env.ps1) is missing"
+    exit 1
 }
 
 . "${PSScriptRoot}/build-and-package-env.ps1"
@@ -26,29 +32,38 @@ if ((Test-Path "${PSScriptRoot}/build-and-package-env.ps1") -ne $True) {
 # At the end of the build return to the directory we started from
 $cwd = Get-Location
 
-### Ensure that OpenVPN and OpenVPN GUI are using the latest dependencies
+### Ensure that we use latest "contrib" vcpkg ports
 cd "${basedir}\openvpn"
 & git.exe pull
 
 cd "${basedir}\vcpkg"
 & git.exe pull
 & .\bootstrap-vcpkg.bat
-& .\vcpkg.exe upgrade --overlay-ports "${basedir}\openvpn\contrib\vcpkg-ports" --overlay-triplets "${basedir}\openvpn\contrib\vcpkg-triplets" --no-dry-run
 
+$architectures = @('x64','x86','arm64')
+ForEach ($arch in $architectures) {
+    # openssl:${arch}-windows is required for openvpn-gui builds
+    & .\vcpkg.exe --overlay-ports "${basedir}\openvpn\contrib\vcpkg-ports" --overlay-triplets "${basedir}\openvpn\contrib\vcpkg-triplets" install --triplet "${arch}-windows-ovpn" lz4 lzo $openssl pkcs11-helper tap-windows6 "${openssl}:${arch}-windows"
+    # Our contrib ports may be more recent that what are available upstream, so
+    # ensure that those are taken into account when upgrading
+    & .\vcpkg.exe --overlay-ports "${basedir}\openvpn\contrib\vcpkg-ports" --overlay-triplets  "${basedir}\openvpn\contrib\vcpkg-triplets" upgrade --no-dry-run
+    & .\vcpkg.exe integrate install
+}
 
 ### Build OpenVPN-GUI
-Copy-Item "${basedir}\openvpn-build\windows-msi\build-openvpn-gui.ps1" "${basedir}\openvpn-gui\"
 cd "${basedir}\openvpn-gui"
+& git.exe pull
+Copy-Item "${basedir}\openvpn-build\windows-msi\build-openvpn-gui.ps1" "${basedir}\openvpn-gui\"
 .\build-openvpn-gui.ps1
-
 
 ### Build OpenVPN
 cd "${basedir}\openvpn"
+& git.exe pull
 
 ForEach ($bat in "msbuild-x64.bat", "msbuild-x64_x86.bat", "msbuild-x64_arm64.bat") {
     If ((Test-Path $bat) -ne $True) {
-		Copy-Item "${basedir}\openvpn-build\windows-msi\${bat}" .
-	}
+        Copy-Item "${basedir}\openvpn-build\windows-msi\${bat}" .
+    }
 }
 
 & .\msbuild-x64.bat
