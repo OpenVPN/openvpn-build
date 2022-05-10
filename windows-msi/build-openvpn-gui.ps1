699a2ee555f6f368b7c067922cf6345ddde197b6
Lev Stipakov <lev@openvpn.net>
2022-05-10 12:02:13 +0300

    windows-msi: adapt to manifest and presets changes in openvpn and openvpn-gui
    With manifest, it is no longer needed to install dependencies separately, they're
    installed as part of build process.
    
    With presets, output directories are already set, no need to create build_<platform>.
    Also CMAKE_TOOLCHAIN_FILE is set by presets from VCPKG_ROOT variable.
    
    Signed-off-by: Lev Stipakov <lev@openvpn.net>

--- windows-msi/build-openvpn-gui.ps1
+++ windows-msi/build-openvpn-gui.ps1
@@ -2,30 +2,21 @@
 #
 # Example of setting the environment variables:
 #
-# $Env:CMAKE_TOOLCHAIN_FILE = "C:\Users\john\vcpkg\scripts\buildsystems\vcpkg.cmake"
+# $Env:VCPKG_ROOT = "C:\Users\john\vcpkg"
 # $Env:CMAKE = "C:\Program Files\CMake\bin\cmake.exe"
 
 $CWD = Get-Location
 $CMAKE=$Env:CMAKE
-$CMAKE_TOOLCHAIN_FILE=$Env:CMAKE_TOOLCHAIN_FILE
+$OSSL=$Env:OSSL
 
 # Enable running this script from anywhere
-cd $PSScriptRoot
+Set-Location $PSScriptRoot
 
-# Architecture names taken from here:
-#
-# https://docs.microsoft.com/en-us/cpp/build/cmake-presets-vs?view=msvc-160
-"x64", "arm64", "Win32" | ForEach  {
+"x64", "arm64", "x86" | ForEach  {
 	$platform = $_
     Write-Host "Building openvpn-gui ${platform}"
-    if ( ( Test-Path "build_${platform}" ) -eq $False )
-	{
-        mkdir build_$platform
-	}
-    cd build_$platform
-    & "$CMAKE" -A $platform -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TOOLCHAIN_FILE" ..
-    & "$CMAKE" --build . --config Release
-    cd $PSScriptRoot
+    & "$CMAKE" -S . --preset ${platform}-release-${OSSL}
+    & "$CMAKE" --build --preset ${platform}-release-${OSSL}
 }
 
-cd $CWD
+Set-Location $CWD
