054e7046c647a05c19e45316d9cb86bc5e7866ae
Lev Stipakov <lev@openvpn.net>
2022-05-25 13:13:49 +0300

    windows-msi: add missing env variables
    Signed-off-by: Lev Stipakov <lev@openvpn.net>

--- windows-msi/build-and-package-env-sample.ps1
+++ windows-msi/build-and-package-env-sample.ps1
@@ -1,5 +1,7 @@
 # Used by build scripts build-and-package.sh calls
 $Env:VCPKG_ROOT = "${basedir}\vcpkg"
+$Env:VCPKG_OVERLAY_PORTS="${basedir}\openvpn-build\windows-msi\vcpkg-ports"
 $Env:CMAKE = "C:\\Program Files\\CMake\\bin\\cmake.exe"
 $Env:ManifestCertificateThumbprint = "thumbprint"
 $Env:ManifestTimestampRFC3161Url = "http://timestamp.digicert.com"
+$Env:OSSL=${ossl}
