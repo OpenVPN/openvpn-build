45b13d79606e78f7ec71d787616974774e78342a
Lev Stipakov <lev@openvpn.net>
2022-05-27 12:26:02 +0300

    windows-msi: support parallel builds
    Add optional -arch parameter to build-and-package.ps1
    which specifies architecture to build.
    
    Remove helper bat files, since after changes to
    openvpn and openvpn-gui build systems they degraded
    to oneliners.
    
    Modify github actions script to enable
    parallel builds.
    
    Signed-off-by: Lev Stipakov <lev@openvpn.net>

--- windows-msi/msbuild-x64_x86.bat
+++
@@ -1,3 +0,0 @@
-rem Building x86 binaries
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64_x86
-msbuild "openvpn.sln" /p:Configuration="Release" /p:Platform="Win32" /maxcpucount /t:Build
