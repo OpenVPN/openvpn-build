8037eb61bf78debef3c45e9c0e365cdd8256f3d0
Lev Stipakov <lev@openvpn.net>
2022-05-27 12:26:02 +0300

    windows-msi: support parallel builds
    Add optional -arch parameter to build-and-package.ps1
    which specifies architecture to build.
    
    Remove helper bat files, since after changes to
    openvpn and openvpn-gui build systems they degraded
    to oneliners.
    
    Modify github actions script to enable
    parallel builds.
    
    Signed-off-by: Lev Stipakov <lev@openvpn.net>

--- windows-msi/build-and-package.ps1
+++ windows-msi/build-and-package.ps1
@@ -4,12 +4,28 @@ param(
     [string] $basedir,
     # Version of OpenSSL port to use ("ossl1.1.1" or "ossl3")
     [string] $ossl = "ossl1.1.1",
-    [switch] $nosign
+    [string] $arch = "all",
+    [switch] $nosign,
+    [switch] $nodevprompt
     )
 
 ### Preparations
 if(-not($basedir)) {
-    Write-Host "Usage: build-and-package.ps1 -basedir <basedir> [-openssl] <ossl1.1.1|ossl3>"
+    Write-Host "Usage: build-and-package.ps1 -basedir <basedir> [-openssl] <ossl1.1.1|ossl3> [-arch] <all|x86|amd64|arm64> [-nosign] [-nodevprompt]"
+    exit 1
+}
+
+$allowed_arch = "all", "x86", "amd64", "arm64"
+if (-Not($allowed_arch.Contains($arch)))
+{
+    Write-Host "-arch must be:" $allowed_arch
+    exit 1
+}
+
+# at the moment signing script doesn't support per-architecture signing
+if (-Not($nosign) -And $arch -ne "all")
+{
+    Write-Host "-arch must be 'all' or omitted when -nosign is not specified"
     exit 1
 }
 
@@ -45,22 +61,62 @@ Set-Location "${basedir}\vcpkg"
 ### Build OpenVPN-GUI
 Set-Location "${basedir}\openvpn-gui"
 & git.exe pull
-Copy-Item "${basedir}\openvpn-build\windows-msi\build-openvpn-gui.ps1" "${basedir}\openvpn-gui\"
-.\build-openvpn-gui.ps1
+
+$gui_arch = @()
+switch ($arch)
+{
+    'all'
+    {
+        $gui_arch = "x64", "arm64", "x86"
+    }
+    'x86'
+    {
+        $gui_arch += "x86"
+    }
+    'amd64'
+    {
+        $gui_arch += "x64"
+    }
+    'arm64'
+    {
+        $gui_arch += "arm64"
+    }
+}
+
+$gui_arch | ForEach-Object  {
+	$platform = $_
+    Write-Host "Building openvpn-gui ${platform}"
+    & "$Env:CMAKE" -S . --preset ${platform}-release-${ossl}
+    & "$Env:CMAKE" --build --preset ${platform}-release-${ossl}
+}
 
 ### Build OpenVPN
 Set-Location "${basedir}\openvpn"
 & git.exe pull
 
-ForEach ($bat in "msbuild-x64.bat", "msbuild-x64_x86.bat", "msbuild-x64_arm64.bat") {
-    If ((Test-Path $bat) -ne $True) {
-        Copy-Item "${basedir}\openvpn-build\windows-msi\${bat}" .
+if (($arch -eq "all") -Or ($arch -eq "amd64")) {
+    if (-not($nodevprompt))
+    {
+        & "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
     }
+    msbuild "openvpn.sln" /p:Configuration="Release" /p:Platform="x64" /maxcpucount /t:Build
 }
 
-& .\msbuild-x64.bat
-& .\msbuild-x64_x86.bat
-& .\msbuild-x64_arm64.bat
+if (($arch -eq "all") -Or ($arch -eq "x86")) {
+    if (-not($nodevprompt))
+    {
+        & "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64_x86
+    }
+    msbuild "openvpn.sln" /p:Configuration="Release" /p:Platform="Win32" /maxcpucount /t:Build
+}
+
+if (($arch -eq "all") -Or ($arch -eq "arm64")) {
+    if (-not($nodevprompt))
+    {
+        & "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64_arm64
+    }
+    msbuild "openvpn.sln" /p:Configuration="Release" /p:Platform="ARM64" /maxcpucount /t:Build
+}
 
 ### Sign binaries
 if (-not $nosign) {
@@ -73,7 +129,26 @@ if (-not $nosign) {
 
 ### Build MSI
 Set-Location "${basedir}\openvpn-build\windows-msi"
-& cscript.exe build.wsf msi
+
+switch ($arch)
+{
+    'all'
+    {
+        & cscript.exe build.wsf msi
+    }
+    'amd64'
+    {
+        & cscript.exe build.wsf msi-amd64
+    }
+    'x86'
+    {
+        & cscript.exe build.wsf msi-x86
+    }
+    'arm64'
+    {
+        & cscript.exe build.wsf msi-arm64
+    }
+}
 
 ### Sign MSI
 if (-not $nosign) {
