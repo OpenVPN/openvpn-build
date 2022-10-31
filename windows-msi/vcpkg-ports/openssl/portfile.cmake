e947ef675ad0356d6798670c08c99426813799ba
Lev Stipakov <lev@openvpn.net>
2022-10-31 14:53:56 +0200

    Bump OpenSSL to 1.1.1s
    Signed-off-by: Lev Stipakov <lev@openvpn.net>

--- windows-msi/vcpkg-ports/openssl/portfile.cmake
+++ windows-msi/vcpkg-ports/openssl/portfile.cmake
@@ -2,12 +2,12 @@ if(EXISTS "${CURRENT_INSTALLED_DIR}/include/openssl/ssl.h")
     message(FATAL_ERROR "Can't build openssl if libressl/boringssl is installed. Please remove libressl/boringssl, and try install openssl again if you need it.")
 endif()
 
-set(OPENSSL_VERSION 1.1.1o)
+set(OPENSSL_VERSION 1.1.1s)
 vcpkg_download_distfile(
     ARCHIVE
     URLS "https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz" "https://www.openssl.org/source/old/1.1.1/openssl-${OPENSSL_VERSION}.tar.gz"
     FILENAME "openssl-${OPENSSL_VERSION}.tar.gz"
-    SHA512 75b2f1499cb4640229eb6cd35d85cbff2e19db17b959ac4d04b60f1b395b73567f9003521452a0fcfeea9b31b26de0a7bccf476ecf9caae02298f3647cfb7e23
+    SHA512 2ef983f166b5e1bf456ca37938e7e39d58d4cd85e9fc4b5174a05f5c37cc5ad89c3a9af97a6919bcaab128a8a92e4bdc8a045e5d9156d90768da8f73ac67c5b9
 )
 
 vcpkg_find_acquire_program(PERL)
