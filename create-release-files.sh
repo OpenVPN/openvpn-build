4fa9b1eb291dc16a90c7010d1a18cdfac39bfc4e
Frank Lichtenheld <frank@lichtenheld.com>
2022-12-02 17:30:55 +0100

    create-release-files: make tar balls and changelogs reproducible
    - use commit date for file timestamps in tar balls
      and for release date in changelog
    - allow to use existing release tag in openvpn-gui
    - we drop the .xz and .zip variants of the source. It is not clear
      that these are required
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- create-release-files.sh
+++ create-release-files.sh
@@ -45,13 +45,16 @@ cd openvpn
 git checkout -B "$OPENVPN_CURRENT_TAG" "$OPENVPN_CURRENT_TAG"
 git pull --rebase origin "$OPENVPN_CURRENT_TAG"
 
+COMMIT_DATE=$(git log --no-show-signature -n1 --format="%cD")
+TAR_REP="tar --sort=name --owner=root:0 --group=root:0"
+
 echo "Creating OpenVPN source packages"
 autoreconf -vi > /dev/null 2>&1
 ./configure > /dev/null
-make dist-gzip > /dev/null
-make dist-zip > /dev/null
-make dist-xz > /dev/null
-
+make distdir > /dev/null
+$TAR_REP --mtime="$COMMIT_DATE" -chf - "openvpn-$OPENVPN_CURRENT_VERSION" \
+    | gzip -c > "openvpn-$OPENVPN_CURRENT_VERSION.tar.gz"
+rm -fr "openvpn-$OPENVPN_CURRENT_VERSION"
 
 # Generate changelog for Trac
 git shortlog "$OPENVPN_PREVIOUS_TAG...refs/tags/$OPENVPN_CURRENT_TAG" \
@@ -59,13 +62,12 @@ git shortlog "$OPENVPN_PREVIOUS_TAG...refs/tags/$OPENVPN_CURRENT_TAG" \
 
 # Create changelog for Debian packages
 DEBIAN_CHANGELOG="$BASEDIR/changelog/openvpn-$OPENVPN_CURRENT_VERSION-changelog-debian"
-DEBIAN_DATE=`date +'%a, %-d %b %Y %X %z'`
-echo "openvpn (${OPENVPN_CURRENT_VERSION}-debian0) stable; urgency=medium" > $DEBIAN_CHANGELOG
-echo >> $DEBIAN_CHANGELOG
+echo "openvpn (${OPENVPN_CURRENT_VERSION}-debian0) stable; urgency=medium" > "$DEBIAN_CHANGELOG"
+echo >> "$DEBIAN_CHANGELOG"
 git log --pretty=short --abbrev-commit --format="  * %s (%an, %h)" \
     "$OPENVPN_PREVIOUS_TAG...refs/tags/$OPENVPN_CURRENT_TAG" >> "$DEBIAN_CHANGELOG"
-echo >> $DEBIAN_CHANGELOG
-echo " -- $GIT_AUTHOR  $DEBIAN_DATE" >> $DEBIAN_CHANGELOG
+echo >> "$DEBIAN_CHANGELOG"
+echo " -- $GIT_AUTHOR  $COMMIT_DATE" >> "$DEBIAN_CHANGELOG"
 
 # Copy the man-page and tarballs
 cp -v doc/openvpn.8.html "$BASEDIR/man/"
@@ -80,16 +82,23 @@ git pull --rebase origin "$OPENVPN_GUI_BRANCH"
 
 # Update minor version in configure.ac
 sed -E -i s/"define\(\[_GUI_VERSION_MINOR\], \[([[:digit:]]+)\]\)"/"define\(\[_GUI_VERSION_MINOR\], \[$OPENVPN_GUI_CURRENT_MIN_VERSION\]\)"/1 configure.ac
-git add configure.ac
-git commit --author="$GIT_AUTHOR" -s -m "Bump version to $OPENVPN_GUI_CURRENT_FULL_VERSION" configure.ac
-git tag -a "v$OPENVPN_GUI_CURRENT_FULL_VERSION" -m "Version $OPENVPN_GUI_CURRENT_FULL_VERSION"
-git tag -a "OpenVPN-$OPENVPN_CURRENT_VERSION-$INSTALLER_VERSION" -m "OpenVPN-$OPENVPN_CURRENT_VERSION-$INSTALLER_VERSION"
+# if configure.ac was already updated, assume everything is fine as is
+if ! git diff --exit-code; then
+    git add configure.ac
+    git commit --author="$GIT_AUTHOR" -s -m "Bump version to $OPENVPN_GUI_CURRENT_FULL_VERSION" configure.ac
+    git tag -a "v$OPENVPN_GUI_CURRENT_FULL_VERSION" -m "Version $OPENVPN_GUI_CURRENT_FULL_VERSION"
+    git tag -a "OpenVPN-$OPENVPN_CURRENT_VERSION-$INSTALLER_VERSION" -m "OpenVPN-$OPENVPN_CURRENT_VERSION-$INSTALLER_VERSION"
+fi
+
+COMMIT_DATE_GUI=$(git log --no-show-signature -n1 --format="%cD")
 
 echo "Creating OpenVPN-GUI source package"
 autoreconf -vi > /dev/null 2>&1
 ./configure --enable-distonly > /dev/null
-make dist-gzip > /dev/null
-cp -v openvpn-gui-$OPENVPN_GUI_CURRENT_MAJ_VERSION.tar.gz "$SOURCES"
+make distdir > /dev/null
+$TAR_REP --mtime="$COMMIT_DATE_GUI" -chf - "openvpn-gui-$OPENVPN_GUI_CURRENT_MAJ_VERSION" \
+    | gzip -c > "openvpn-gui-$OPENVPN_GUI_CURRENT_MAJ_VERSION.tar.gz"
+cp -v "openvpn-gui-$OPENVPN_GUI_CURRENT_MAJ_VERSION.tar.gz" "$SOURCES"
 cd "$BASEDIR"
 
 cd "$SOURCES"
