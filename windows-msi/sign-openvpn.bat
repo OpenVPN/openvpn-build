d2979abeef8ec6806de60ad7e46c0c4099ec88a7
Marc Becker <becm@gmx.de>
2022-12-29 17:42:02 +0100

    fix line continuation in sign-openvpn.bat
    Include `openvpn-gui` x86 DLLs during sign operation

--- windows-msi/sign-openvpn.bat
+++ windows-msi/sign-openvpn.bat
@@ -27,5 +27,5 @@ signtool.exe sign /sha1 "%ManifestCertificateThumbprint%" /fd sha256 /tr "%Manif
  ..\src\openvpn-gui\out\build\x64-release-"%OSSL%"\*.dll^
  ..\src\openvpn-gui\out\build\arm64-release-"%OSSL%"\openvpn-gui.exe^
  ..\src\openvpn-gui\out\build\arm64-release-"%OSSL%"\*.dll^
- ..\src\openvpn-gui\out\build\x86-release-"%OSSL%"\openvpn-gui.exe
- ..\src\openvpn-gui\out\build\x86-release-"%OSSL%"\*.dll^
+ ..\src\openvpn-gui\out\build\x86-release-"%OSSL%"\openvpn-gui.exe^
+ ..\src\openvpn-gui\out\build\x86-release-"%OSSL%"\*.dll
