b06a1b12f3ef0631fab176193a6e55fcf1fca809
Frank Lichtenheld <frank@lichtenheld.com>
2023-01-26 12:17:36 +0100

    release/windows-installer-build.sh: Some fixes for installer-only rebuild
    - Do not fail on HSM service already running
    - Do not fail on missing local upload directory
    - Only download the MSIs we're interested in
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- release/windows-installer-build.sh
+++ release/windows-installer-build.sh
@@ -21,7 +21,7 @@ pushd "$TOP_DIR"
 . "$SCRIPT_DIR/vars"
 . "$SCRIPT_DIR/vars.infrastructure"
 
-#TODO: make idempotent, check whether password is required
+ssh $WINDOWS_MSI_BUILDHOST "net.exe stop AWSCloudHSMClient" || true
 ssh $WINDOWS_MSI_BUILDHOST "\"C:\Program Files\Amazon\CloudHSM\configure.exe\" -a $HSM_IP && net.exe start AWSCloudHSMClient"
 ssh $WINDOWS_MSI_BUILDHOST "env n3fips_password=${HSM_USER:-cuuser}:$HSM_PASSWORD \"C:\Program Files\Amazon\CloudHSM\import_key.exe\" -from HSM -privateKeyHandle $HSM_PRIV_KEY_HANDLE -publicKeyHandle $HSM_PUB_KEY_HANDLE"
 ssh $WINDOWS_MSI_BUILDHOST "env n3fips_password=${HSM_USER:-cuuser}:$HSM_PASSWORD certutil -f -csp \"Cavium Key Storage Provider\" -user -repairstore my $WINDOWS_SIGNING_KEY_FP"
@@ -35,7 +35,8 @@ ssh $WINDOWS_MSI_BUILDHOST git -C "$WINDOWS_MSI_WORKDIR" checkout --recurse-subm
 ssh $WINDOWS_MSI_BUILDHOST "cd $WINDOWS_MSI_WORKDIR/windows-msi && echo \$Env:ManifestCertificateThumbprint = \"$WINDOWS_SIGNING_KEY_FP\" >build-and-package-env.ps1"
 ssh $WINDOWS_MSI_BUILDHOST "cd $WINDOWS_MSI_WORKDIR/windows-msi && \"C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat\" x64 && env n3fips_password=${HSM_USER:-cuuser}:$HSM_PASSWORD powershell ./build-and-package.ps1 -sign"
 
-scp "$WINDOWS_MSI_BUILDHOST":$WINDOWS_MSI_WORKDIR/windows-msi/image/*.msi "$OUTPUT/upload/"
+mkdir -p "$OUTPUT/upload/"
+scp "$WINDOWS_MSI_BUILDHOST":"$WINDOWS_MSI_WORKDIR/windows-msi/image/OpenVPN-${BUILD_VERSION}"-*.msi "$OUTPUT/upload/"
 read -p "Upload MSIs to $SECONDARY_WEBSERVER?"
 # upload MSIs
 $SCRIPT_DIR/sign-and-push.sh
